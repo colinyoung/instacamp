377e79cee41a599c44bae1cd07c08662a8e0887f
Colin Young <me@colinyoung.com>
2011-01-22 23:36:38 -0600

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Instacamp::Application.config.secret_token = 'b6d2199d22a9f4bb5c163cf395c3dcc6fae8850e84c2a8d5190f827e3d48e427b19d67ab9285c9a1e7d5369cbf910f74d10b52e0b3cca4d793acc6581119da8d'
